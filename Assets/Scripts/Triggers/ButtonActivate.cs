44a5394d3459ad3ee42e02b79c430c90b21a0f37
Nick <nakort@umich.edu>
2017-10-26 01:22:49 -0400

    doors and switches

---
+++ Assets/Scripts/Triggers/ButtonActivate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivate : MonoBehaviour {
+
+    public GameObject TriggeredObject;
+
+    Renderer render;
+    Color oldColor;
+    Color activeColor = Color.cyan;
+
+    // Use this for initialization
+    void Start()
+    {
+        render = this.GetComponent<Renderer>();
+        oldColor = render.material.color;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TriggeredObject.SetActive(false);
+        render.material.color = activeColor;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        TriggeredObject.SetActive(true);
+        render.material.color = oldColor;
+    }
+}
---
+++ Assets/Scripts/Triggers/SwitchLatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLatch : MonoBehaviour {
+
+    public GameObject TriggeredObject;
+
+    Renderer render;
+    Color activeColor = Color.cyan;
+
+	// Use this for initialization
+	void Start () {
+        render = this.GetComponent<Renderer>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TriggeredObject.SetActive(false);
+        render.material.color = activeColor;
+    }
+}
