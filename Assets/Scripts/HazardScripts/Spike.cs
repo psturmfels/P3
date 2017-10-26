20aa106830d46ecb58ec5a35859a63ac44878a87
Cem Akdemir <cem.akd@hotmail.com>
2017-10-25 21:37:00 -0400

    Spike prefab and Level 1

---
+++ Assets/Scripts/HazardScripts/Spike.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetComponent<MellowStates>() != null) {
+            other.gameObject.GetComponent<MellowCrushed>().Die();
+        }
+    }
+}
