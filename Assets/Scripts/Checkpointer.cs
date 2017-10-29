f072d608d2e856b339f68e561a6006a979395bfa
Cem Akdemir <cem.akd@hotmail.com>
2017-10-29 13:28:41 -0400

    Chekpoint cemra reassigning

--- Assets/Scripts/Checkpointer.cs
+++ Assets/Scripts/Checkpointer.cs
@@ -36,12 +36,25 @@ public class Checkpointer : MonoBehaviour {
     public void ResetToCheckpoint() {
         Destroy(this.gameObject);
         Instantiate(self, CheckpointPos, Quaternion.identity);
+        ResetCameraReferences();
         //reset other mello
-        
+
     }
 
     public void ResetToBeginning() {
         //reset other mello
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void ResetCameraReferences() {
+        CameraMovement cm = Camera.main.gameObject.GetComponent<CameraMovement>();
+        if (gameObject.name == "BridgeMellow") {
+            cm.player1Transformed = transform.GetChild(0);
+            cm.player1 = transform.GetChild(1);
+        }
+        else {
+            cm.player2Transformed = transform.GetChild(0);
+            cm.player2 = transform.GetChild(1);
+        }
     }
 }
