e4d27e8d693bced0f9a5a351c8fe51cb425a54a2
psturmfels <psturm@umich.edu>
2017-11-01 22:10:55 -0400

    minor fixes to mellow jumping

--- Assets/Scripts/MellowStates/StateToggle.cs
+++ Assets/Scripts/MellowStates/StateToggle.cs
@@ -6,6 +6,7 @@ public class StateToggle : MonoBehaviour {
 	public MellowStates.State toggleState;
 	public bool OnTriggerEnable;
 	public bool RegisterStay;
+	public bool DebugCollisions = false;
 
 	private MellowStates ms;
 
@@ -17,17 +18,26 @@ public class StateToggle : MonoBehaviour {
 		if (ms != null) {
 			ms.SetState (toggleState, OnTriggerEnable);
 		}
+		if (DebugCollisions) {
+			Debug.Log ("Entered trigger with: " + other.gameObject.name);
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D other) {
 		if (ms != null && RegisterStay) {
 			ms.SetState (toggleState, OnTriggerEnable);
 		}
+		if (DebugCollisions) {
+			Debug.Log ("Stayed trigger with: " + other.gameObject.name);
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (ms != null) {
 			ms.SetState (toggleState, !OnTriggerEnable);
+		}
+		if (DebugCollisions) {
+			Debug.Log ("Left trigger with: " + other.gameObject.name);
 		}
 	}
 }
