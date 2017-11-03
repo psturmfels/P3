a2959b9fc1fe34727cb1dbe5cf34c5223f88a559
psturmfels <psturm@umich.edu>
2017-11-03 08:56:52 -0400

    destroy death explosion on complete

---
+++ Assets/Scripts/MellowAnimation/DestroyParticleSystemOnComplete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleSystemOnComplete : MonoBehaviour {
+	private ParticleSystem ps;
+
+	void Start () {
+		ps = GetComponent<ParticleSystem> ();
+	}
+
+	void Update () {
+		if(ps)
+		{
+			if(!ps.IsAlive())
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
