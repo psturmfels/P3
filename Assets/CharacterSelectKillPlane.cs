3109b096bd7071d22e25c4cf673cbd5001fde7ec
Jack Park <parkjack@umich.edu>
2017-11-03 19:39:17 -0400

    pushing player character select screen

---
+++ Assets/CharacterSelectKillPlane.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectKillPlane : MonoBehaviour
+{
+    Vector3 Player1OriginalPosition;
+    Vector3 Player2OriginalPosition;
+
+    private void Start()
+    {
+        Player1OriginalPosition = GameObject.Find("BridgeMellowMove").GetComponent<Transform>().position;
+        Player2OriginalPosition = GameObject.Find("StiltMellowMove").GetComponent<Transform>().position;
+    }
+
+    //Check if the thing colliding is player 1 or two, and move if necessary.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.name == "BridgeMellowMove")
+        {
+            collision.collider.transform.position = Player1OriginalPosition;
+        }
+
+        else if(collision.collider.gameObject.name == "StiltMellowMove")
+        {
+            collision.collider.transform.position = Player2OriginalPosition;
+        }
+    }
+}
