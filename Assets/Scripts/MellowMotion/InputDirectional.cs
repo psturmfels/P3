6b72bec1411cd0268bac2d74ec71cc2dccc61110
Cem Akdemir <cem.akd@hotmail.com>
2017-11-10 01:54:20 -0500

    Slide behavior initial push, incomplete

---
+++ Assets/Scripts/MellowMotion/InputDirectional.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDirectional : MonoBehaviour {
+
+    private int playerID = 0;
+    private PlayerDeviceManager deviceManager;
+    private float horizontalAxis;
+    private float verticalAxis;
+
+    // Use this for initialization
+    void Start() {
+        //Find PlayerDeviceManager
+        deviceManager = GameObject.Find("PlayerDeviceManager").GetComponent<PlayerDeviceManager>();
+    }
+
+    public void SetCurrentHorzAxis(float newHorzAxis) {
+        horizontalAxis = newHorzAxis;
+    }
+
+    public float GetCurrentHorzAxis() {
+        return horizontalAxis;
+    }
+
+    void Update() {
+        //Find the controls bound to this player
+        if (deviceManager != null) {
+            PlayerActions controls = deviceManager.GetControls(playerID);
+            if (controls != null) {
+                SetCurrentHorzAxis(controls.Move.X);
+            }
+            else {
+                SetCurrentHorzAxis(0.0f);
+            }
+        }
+    }
+}
