ed1be19b1cfa4fb48ed6dda241fe13a9468a0618
psturmfels <psturm@umich.edu>
2017-11-10 14:53:58 -0500

    added parallax

---
+++ Assets/Scripts/CameraScripts/MoveWithCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithCamera : MonoBehaviour {
+	public float moveFraction = 1.0f;
+
+	private Camera mainCamera;
+	private Vector2 previousCameraPosition;
+
+	void Start () {
+		mainCamera = Camera.main;	
+		previousCameraPosition = mainCamera.transform.position;
+	}
+
+	void Update () {
+		if (mainCamera != null) {
+			Vector2 currentCameraPosition = new Vector2 (mainCamera.transform.position.x, mainCamera.transform.position.y);
+			Vector2 difference = currentCameraPosition - previousCameraPosition;
+			Vector3 targetPosition = transform.position + new Vector3 (difference.x, difference.y, 0.0f);
+			transform.position = Vector3.Lerp (transform.position, targetPosition, moveFraction);
+			previousCameraPosition = currentCameraPosition;
+		}
+	}
+}
