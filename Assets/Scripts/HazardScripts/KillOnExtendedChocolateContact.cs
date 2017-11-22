bb11709a05824d683e5f8a6b29ae2a792fe6c513
psturmfels <psturm@umich.edu>
2017-11-22 14:31:46 -0500

    getting stuck in chocolate kills you

---
+++ Assets/Scripts/HazardScripts/KillOnExtendedChocolateContact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnExtendedChocolateContact : MonoBehaviour {
+	private MellowCrushed mc;
+	public float waitTime;
+
+	void Start () {
+		mc = GetComponentInParent<MellowCrushed> ();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log (other.gameObject.name);
+		if (other.gameObject.GetComponentInParent<Door> () != null) {
+			StartCoroutine (KillPlayer (waitTime));
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.GetComponentInParent<Door> () != null) {
+			StopAllCoroutines ();
+		}
+	}
+
+	IEnumerator KillPlayer(float waitTime) {
+		yield return new WaitForSeconds (waitTime);
+		mc.StartDie ();
+	}
+}
