e5a47e63c66a4e2c6faafa2fe917c17b9439c4de
Cem Akdemir <cem.akd@hotmail.com>
2017-11-26 16:20:07 -0500

    level shifting help text

--- Assets/Scripts/System/SpriteFadeInOnTrigger.cs
+++ Assets/Scripts/System/SpriteFadeInOnTrigger.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class SpriteFadeInOnTrigger : MonoBehaviour {
 
     private SpriteRenderer buttonSr;
-    private SpriteRenderer sr;
+    private SpriteRenderer[] childSrs;
     private float fadeIncrement = 0.025f;
     private int numMellows = 0;
 
     void Start()
     {
         buttonSr = GetComponent<SpriteRenderer>();
-        sr = GetComponentsInChildren<SpriteRenderer>()[1];
+        childSrs = GetComponentsInChildren<SpriteRenderer>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -41,32 +42,60 @@ public class SpriteFadeInOnTrigger : MonoBehaviour {
         }
     }
 
-    IEnumerator FadeOut()
-    {
-        while (buttonSr.color.a > 0.0f)
-        {
-            Color buttonColor = buttonSr.color;
-            buttonColor.a = Mathf.Max(buttonColor.a - fadeIncrement, 0.0f);
-            buttonSr.color = buttonColor;
+    IEnumerator FadeOut() {
+        bool mainButtonDone = false;
+        bool childrenDone = false;
+        while (!mainButtonDone && !childrenDone) {
+            if (buttonSr != null) {
+                if (buttonSr.color.a > 0f) {
+                    mainButtonDone = true;
+                }
+                Color buttonColor = buttonSr.color;
+                buttonColor.a = Mathf.Max(buttonColor.a - fadeIncrement, 0.0f);
+                buttonSr.color = buttonColor;
+            }
 
-            Color panelColor = sr.color;
-            panelColor.a = Mathf.Max(panelColor.a - fadeIncrement, 0.0f);
-            sr.color = panelColor;
+            if (childSrs != null) {
+                bool allChildrenDone = true;
+                foreach (var sr in childSrs) {
+                    if (sr.color.a > 0f) {
+                        allChildrenDone = false;
+                    }
+                    Color panelColor = sr.color;
+                    panelColor.a = Mathf.Max(panelColor.a - fadeIncrement, 0.0f);
+                    sr.color = panelColor;
+                }
+                childrenDone = allChildrenDone;
+            }
             yield return null;
         }
     }
 
-    IEnumerator FadeIn()
-    {
-        while (buttonSr.color.a < 0.8f)
-        {
-            Color buttonColor = buttonSr.color;
-            buttonColor.a = Mathf.Min(buttonColor.a + fadeIncrement, 0.8f);
-            buttonSr.color = buttonColor;
+    IEnumerator FadeIn() {
+        bool mainButtonDone = false;
+        bool childrenDone = false;
+        while (!mainButtonDone && !childrenDone) {
+            if (buttonSr != null) {
+                if (buttonSr.color.a > 0.8f) {
+                    mainButtonDone = true;
+                }
+                Color buttonColor = buttonSr.color;
+                buttonColor.a = Mathf.Min(buttonColor.a + fadeIncrement, 0.8f);
+                buttonSr.color = buttonColor;
+            }
 
-            Color panelColor = sr.color;
-            panelColor.a = Mathf.Min(panelColor.a + fadeIncrement, 0.8f);
-            sr.color = panelColor;
+            if (childSrs != null) {
+                bool allChildrenDone = true; 
+                foreach (var sr in childSrs) {
+                    if (sr.color.a < 0.8f) {
+                        allChildrenDone = false;
+                    }
+                    Color panelColor = sr.color;
+                    panelColor.a = Mathf.Min(panelColor.a + fadeIncrement, 0.8f);
+                    sr.color = panelColor;
+                }
+                childrenDone = allChildrenDone;
+            }
             yield return null;
         }
     }
