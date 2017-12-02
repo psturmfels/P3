ee86c0c1cf17469dcfefef48184c141d489194ba
Cem Akdemir <cem.akd@hotmail.com>
2017-12-02 13:54:55 -0500

    Candy wave enables automatically by default now

---
+++ Assets/Scripts/System/EnableOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnLoad : MonoBehaviour {
+    public GameObject[] objects;
+
+	// Use this for initialization
+	void Start () {
+	    foreach (var obj in objects) {
+	        obj.SetActive(true);
+	    }
+	}
+}
