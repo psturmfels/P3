4155ea2440155075be79fb7fdfb69b332898c567
psturmfels <psturm@umich.edu>
2017-12-08 16:53:52 -0500

    added camera panning on switch

--- Assets/Scripts/Triggers/SwitchLatch.cs
+++ Assets/Scripts/Triggers/SwitchLatch.cs
@@ -9,6 +9,8 @@ public class SwitchLatch : MonoBehaviour {
     public Sprite switchTriggeredSprite;
 
     public AudioSource leverSound;
+	public bool lerpBeforeUnlocking = false;
+	public GameObject lerpObjectPos = null;
 
     private SpriteRenderer sr;
 	private bool hasBeenSwitched = false;
@@ -33,7 +35,19 @@ public class SwitchLatch : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
 		if (!hasBeenSwitched) {
-			OnSwitchTrigger ();
+			if (lerpBeforeUnlocking && lerpObjectPos != null) {
+				if (Camera.main != null && Camera.main.GetComponent<CameraMovement> () != null) {
+					CameraMovement cm = Camera.main.GetComponent<CameraMovement> ();
+					Vector2 doorPosition = new Vector2 (lerpObjectPos.transform.position.x, lerpObjectPos.transform.position.y);
+					sr.sprite = switchTriggeredSprite;
+					leverSound.Play ();
+					cm.StartLerpToDoorUnlock (doorPosition, this);
+				} else {
+					OnSwitchTrigger ();
+				}
+			} else {
+				OnSwitchTrigger ();
+			}
 		}
     }
 
