61326ef0517a25bacc436deb4e07d3eeaee8fac1
parkjack <parkjack@umich.edu>
2017-12-08 16:44:47 -0500

    pushing zero colliders

---
+++ Assets/ZeroCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroCollider : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        this.transform.position = Vector3.zero;
+    }
+}
