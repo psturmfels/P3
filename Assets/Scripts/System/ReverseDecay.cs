ac1c2f3936c7d3fdc18b6b19a8d20bdecc55143c
parkjack <parkjack@umich.edu>
2017-12-08 19:13:04 -0500

    level transitions

--- Assets/Scripts/System/ReverseDecay.cs
+++ Assets/Scripts/System/ReverseDecay.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 
 public class ReverseDecay : MonoBehaviour {
 
+    public Sprite RedColumn;
+    public Sprite GreenColumn;
+    public Sprite BlueColumn;
+    public Sprite RainbowColumn;
+
     public void ReverseWaveDecay() {
         var alphaDecays = GetComponentsInChildren<AlphaDecay>();
 
         foreach(var decay in alphaDecays) {
             decay.ReverseDecay();
+        }
+    }
+
+    public void ReplaceSprite(string color)
+    {
+        var childSpriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+
+        foreach(var target in childSpriteRenderers)
+        {
+            if(color == "Red")
+            {
+                target.sprite = RedColumn;
+            }
+
+            if(color == "Green")
+            {
+                target.sprite = GreenColumn;
+            }
+
+            if(color == "Blue")
+            {
+                target.sprite = BlueColumn;
+            }
+
+            if(color == "Rainbow")
+            {
+                target.sprite = RainbowColumn;
+            }
+
+            else
+            {
+                target.sprite = RainbowColumn;
+            }
         }
     }
 }
